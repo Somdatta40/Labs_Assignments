e985c881d08e9f19e69b1e40deb241c21b0209ad
Somdatta40 <84366624+Somdatta40@users.noreply.github.com>
2021-05-19 17:52:15 +0530

    compulsory lab4

---
+++ lab4Compulsary/Book.java
@@ -0,0 +1,7 @@
+package lab4Compulsary;
+
+public class Book extends WrittenItem {
+	public Book(int identificationNumber, String title, int numberOfCopies, String author) {
+		super(identificationNumber,title,numberOfCopies,author);
+	}
+}
